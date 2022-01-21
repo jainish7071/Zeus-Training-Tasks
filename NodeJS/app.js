ee6091be9429592f477872bbd8fc5535147ec4c5
Jainish Kunjadiya <jainishkunjadiya2712@gmail.com>
2022-01-21 12:08:05 +0530

    NodeJS Session And Cookie completed

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,23 +1,53 @@
 const express = require("express");
+const sessions = require("express-session");
+const cookieParser = require("cookie-parser");
 const con = require("./db_connect.js");
 const app = express();
-
+app.use(express.json());
+app.use(
+  sessions({
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+    resave: false,
+  })
+);
+app.use(cookieParser());
 app.get("/", (req, res) => {
+  if (req.session.username && req.cookies["username"]) {
+    return res.status(200).send(req.session.username + " " + req.cookies["username"]);
+  } else if (req.cookies["username"]) {
+    return res.status(200).send(req.cookies["username"]);
+  }
   res.status(200).send("this is starting of my first page using nodejs");
 });
 
-app.get("/users", async (req, res) => {
-  con.query("SELECT * FROM customers", (err, result) => {
+app.get("/users", (req, res) => {
+  con.query("SELECT * FROM customers", (err, result, fields) => {
     if (err) console.log(err);
     else res.status(200).send(result);
   });
 });
-
-app.get("/user/:id", async (req, res) => {
+app.get("/user/:id", (req, res) => {
   con.query("SELECT * FROM customers WHERE customer_id = ?", req.params.id, (err, result) => {
+    if (err) console.log(err);
+    else {
+      req.session.username = result[0].first_name;
+      res.cookie("username", result[0].last_name);
+      res.status(200).send(result);
+    }
+  });
+});
+app.post("/user/:id", (req, res) => {
+  con.query(`UPDATE customers SET ? WHERE customer_id = ${req.params.id}`, req.body, (err, result) => {
     if (err) console.log(err);
     else res.status(200).send(result);
   });
+});
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.cookie("username", "", { maxAge: -1000 });
+  res.redirect("/");
 });
 
 let port = process.env.HOST || 80;
